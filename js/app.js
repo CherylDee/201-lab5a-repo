cf3543153c0cefcf82464ed6c92f9fac2c2bf0ab
Cheryl McCalop <105820519+CherylDee@users.noreply.github.com>
2022-12-11 17:00:22 -0800

    Update app.js
    added code for problem 4

--- js/app.js
+++ js/app.js
@@ -82,7 +82,11 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let firstSum = sum(sumArr[0], sumArr[1])[0];
+  let nextSum = sum(firstSum, sumArr[2])[0];
+  let arrSumm = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+  
+  return [nextSum, `${sumArr} was passed in as an array of numbers, and ${nextSum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
